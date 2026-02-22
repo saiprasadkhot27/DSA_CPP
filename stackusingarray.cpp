815f1d7d6b9dc79a4ccda653cf2fbd6486e83085
saiprasadkhot27 <saiprasadkhot27@gmail.com>
2026-02-22 19:00:17 +0530

    Create stackusingarray.cpp

---
+++ stackusingarray.cpp
@@ -0,0 +1,78 @@
+```cpp
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+class StackArray {
+private:
+    int arr[MAX];
+    int top;
+
+public:
+    // Constructor
+    StackArray() {
+        top = -1;
+    }
+
+    bool isEmpty() {
+        return (top == -1);
+    }
+
+    bool isFull() {
+        return (top == MAX - 1);
+    }
+
+    void push(int x) {
+        if (isFull()) {
+            cout << "Stack Overflow\n";
+            return;
+        }
+        arr[++top] = x;
+        cout << x << " pushed into stack\n";
+    }
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack Underflow\n";
+            return;
+        }
+        cout << arr[top--] << " popped from stack\n";
+    }
+
+    void peek() {
+        if (isEmpty()) {
+            cout << "Stack is empty\n";
+            return;
+        }
+        cout << "Top element: " << arr[top] << endl;
+    }
+
+    void display() {
+        if (isEmpty()) {
+            cout << "Stack is empty\n";
+            return;
+        }
+        cout << "Stack elements: ";
+        for (int i = top; i >= 0; i--) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    StackArray s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.display();
+
+    s.peek();
+    s.pop();
+    s.display();
+
+    return 0;
+}
+```
