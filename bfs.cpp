1d35a0b1efa76ab878bbc25922c0a6607fdbe1c5
saiprasadkhot27 <saiprasadkhot27@gmail.com>
2026-05-08 11:41:02 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 using namespace std;
 
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> adj(n);
-    for(int i = 0; i < m; i++) {
+
+    int adj[100][100] = {0};
+
+    
+    for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // for undirected graph
     }
-    vector<bool> visited(n, false);
-    queue<int> q;
+
     int start;
     cin >> start;
-    visited[start] = true;
-    q.push(start);
-    while(!q.empty()) {
-        int node = q.front();
-        q.pop();
+
+    int visited[100] = {0};
+
+    int queue[100];
+    int front = 0, rear = 0;
+
+    visited[start] = 1;
+    queue[rear++] = start;
+
+    cout << "BFS Traversal: ";
+
+    while (front < rear) {
+        int node = queue[front++];
         cout << node << " ";
-        for(int neighbor : adj[node]) {
-            if(!visited[neighbor]) {
-                visited[neighbor] = true;
-                q.push(neighbor);
+
+        for (int i = 0; i < n; i++) {
+            if (adj[node][i] == 1 && visited[i] == 0) {
+                visited[i] = 1;
+                queue[rear++] = i;
             }
         }
     }
+
     return 0;
 }
