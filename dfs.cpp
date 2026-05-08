b2e7bee54eaeee58ec8059f85627695a4da3cf12
saiprasadkhot27 <saiprasadkhot27@gmail.com>
2026-05-08 11:41:51 +0530

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -1,36 +1,42 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int main() {
-    int states, symbols;
-    cin >> states >> symbols;
-    vector<vector<int>> transition(states, vector<int>(symbols));
-    for(int i = 0; i < states; i++) {
-        for(int j = 0; j < symbols; j++) {
-            cin >> transition[i][j];
+int adj[100][100];
+int visited[100];
+int n;
+
+void dfs(int node) {
+    visited[node] = 1;
+
+    cout << node << " ";
+
+    for (int i = 0; i < n; i++) {
+        if (adj[node][i] == 1 && visited[i] == 0) {
+            dfs(i);
         }
     }
+}
+
+int main() {
+    int m;
+
+    cin >> n >> m;
+
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u][v] = 1;
+        adj[v][u] = 1; // for undirected graph
+    }
+
     int start;
     cin >> start;
-    int acceptCount;
-    cin >> acceptCount;
-    vector<bool> accept(states, false);
-    for(int i = 0; i < acceptCount; i++) {
-        int x;
-        cin >> x;
-        accept[x] = true;
-    }
-    string input;
-    cin >> input;
-    int current = start;
-    for(char c : input) {
-        int symbol = c - '0';
-        current = transition[current][symbol];
-    }
-    if(accept[current])
-        cout << "Accepted";
-    else
-        cout << "Rejected";
+
+    cout << "DFS Traversal: ";
+
+    dfs(start);
+
     return 0;
 }
